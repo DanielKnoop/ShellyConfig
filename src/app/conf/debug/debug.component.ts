6306acb64ea08132f0f474c0fd043f3009bf38f5
Daniel Knoop <daniel@knoop.in>
2021-05-27 08:53:46 +0200

    debugging

--- src/app/conf/debug/debug.component.ts
+++ src/app/conf/debug/debug.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { CollectionViewer, DataSource } from '@angular/cdk/collections';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
+import {BehaviorSubject, Observable, Subscription} from 'rxjs';
+
+class Message {
+  constructor() {}
+}
+
+class Messages {
+  constructor() {
+    this.messageArray = new Array<Message>();
+  }
+  messageArray: Message[];
+}
+
+export class myDataSource extends DataSource<string | undefined> {
+  private _length = 100000;
+  private _pageSize = 100;
+  private _cachedData = Array.from<string>({length: this._length});
+  private _fetchedPages = new Set<number>();
+  private readonly _dataStream = new BehaviorSubject<(string | undefined)[]>(this._cachedData);
+  private readonly _subscription = new Subscription();
+
+  connect(collectionViewer: CollectionViewer): Observable<(string | undefined)[]> {
+    this._subscription.add(collectionViewer.viewChange.subscribe(range => {
+      const startPage = this._getPageForIndex(range.start);
+      const endPage = this._getPageForIndex(range.end - 1);
+      for (let i = startPage; i <= endPage; i++) {
+        this._fetchPage(i);
+      }
+    }));
+    return this._dataStream;
+  }
+
+  disconnect(): void {
+    this._subscription.unsubscribe();
+  }
+  private _getPageForIndex(index: number): number {
+    return Math.floor(index / this._pageSize);
+  }
+
+  private _fetchPage(page: number) {
+    if (this._fetchedPages.has(page)) {
+      return;
+    }
+    this._fetchedPages.add(page);
+
+    // Use `setTimeout` to simulate fetching data from server.
+    setTimeout(() => {
+      this._cachedData.splice(page * this._pageSize, this._pageSize,
+          ...Array.from({length: this._pageSize})
+              .map((_, i) => `Item #${page * this._pageSize + i}`));
+      this._dataStream.next(this._cachedData);
+    }, Math.random() * 1000 + 200);
+  }
+
+}
 
 @Component({
   selector: 'app-debug',
   templateUrl: './debug.component.html',
-  styleUrls: ['./debug.component.css']
+  styleUrls: ['./debug.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DebugComponent implements OnInit {
+
+export class DebugComponent implements OnInit, OnDestroy {
+  myWebSocket: WebSocketSubject<any> = webSocket('ws://192.168.199.41:7681/connection');
 
   constructor() { }
 
+  ngOnDestroy(): void {
+    this.myWebSocket.unsubscribe();
+  }
+
   ngOnInit(): void {
+    this.myWebSocket.subscribe(
+      msg => console.log('message received: ' + JSON.stringify(msg)),
+      // Called whenever there is a message from the server
+      err => console.log(err),
+      // Called if WebSocket API signals some kind of error
+      () => console.log('Websocket Disconnected')
+      // Called when connection is closed (for whatever reason)
+    );
   }
 
 }
